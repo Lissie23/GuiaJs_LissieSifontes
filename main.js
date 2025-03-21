63be8c47a7ab13ffec93faa120b08a9c276ed99b
Gabo@DESKTOP-P83O0FL.(none) <lissie.sifo@gmail.com>
2025-03-20 22:00:41 -0600

    :sparkles:Complete exercise two

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ for (let i = 1; i <= 10; i++){
     console.log(i);
 }
 
+//Ejercicio 1.2: Imprime los números pares entre 0 y 20 usando for y una condición if.
+for (let i = 0; i <= 20; i++){
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+
